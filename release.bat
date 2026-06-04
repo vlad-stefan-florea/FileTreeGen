6a9bdc32145326b8ffe6781b0e8893662e2b9591
Florea Vlad <florea.vladstefan.dev@gmail.com>
2026-06-04 17:58:32 +0300

    Removed personal scripts from repository tracking

--- release.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set "MODE=%~1"
-set "VERSION=%~2"
-powershell -ExecutionPolicy Bypass -File "%~dp0release_script.ps1" -Mode "%MODE%" -CustomVersion "%VERSION%"
-start "" "%~dp0releases"
